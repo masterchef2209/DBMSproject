3ee472b41598ae322dc97a4d3dbcdf10ae2c3ad1
SwastikSingh0301 <swastik.singh0301@gmail.com>
2019-03-11 01:59:44 +0530

    add-new-course

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ var passport                       = require('passport');
 const GoogleStrategy               = require('passport-google-oauth20').Strategy;
 const session                      = require('express-session');
 const cookieParser                 = require('cookie-parser');
+const mysql                        = require('mysql');
 
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '<YOUR PASSWORD>',
+    database : 'meraki'
+  });
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
 
 // set view engine
 app.set('view engine', 'ejs');
@@ -46,6 +62,33 @@ app.get("/login", function (req, res){
     res.send("login page");
 });
 
+app.get("/add-new-course", (req,res)=>{
+    res.render("add-new-course");
+})
+
+app.post("/add-new-course", (req,res)=>{
+    var title      = req.body.title;
+    var tag        = req.body.tag[0];
+    var link       = req.body.link;
+    var instructor = req.body.instructor;
+    var price      = req.body.price;
+    var query = "INSERT INTO courses (title, tag, link, instructor, price) VALUES ('" + title + "'" + ", '" + tag +"'"+ ", '" + link +"'"+ ", '" + instructor +"'"+ ", '" + price +"'"+ ")";
+    connection.query(query, function (error, results, fields) {
+        if (error) throw error;
+        else{
+            res.redirect ("/add-new-course")
+        }
+    });
+    
+    
+})
+
+// example of query action
+// connection.query('SELECT * FROM `books` WHERE `author` = "David"', function (error, results, fields) {
+//     // error will be an Error if one occurred during the query
+//     // results will contain the results of the query
+//     // fields will contain information about the returned results fields (if any)
+//   });
 
 app.get("/search", function (req, res){
     res.send("here we will show search results");
